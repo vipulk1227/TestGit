514d01b77e6d6ecbaf5e42f4f90e2ae1c8c951ea
vipulkhutale12 <vipulkhutale1212>
2020-09-10 10:54:30 +0530

    hi

--- SeleniumWebdriver/src/com/packages/ElementLocatorPath.java
+++ SeleniumWebdriver/src/com/packages/ElementLocatorPath.java
@@ -14,15 +14,20 @@ public class ElementLocatorPath {
      driver.get("https://www.amazon.in/");
      
      // Finding particular product
-    WebElement searchbox=driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-     searchbox.click();
-     searchbox.sendKeys("t-shirt");
-     WebElement submit=driver.findElement(By.xpath("//input[@type='submit' and @class='nav-input']"));
-     submit.click();
+    /*WebElement searchbox=driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+      searchbox.sendKeys("iphone");
      
+     WebElement searchButton=driver.findElement(By.xpath("//input[@type='submit' and @class='nav-input']"));
+      searchButton.click();*/
+      
+    WebElement accountLink=driver.findElement(By.xpath("//span[@class='nav-line-2 nav-short-width']"));
+      accountLink.click();
      
-     
-     
+    WebElement loginField=driver.findElement(By.xpath("//input[@id='ap_email']"));
+      //WebElement loginField=driver.findElement(By.name("email"));
+      
+      loginField.sendKeys("vipul@gmail.com");
+      
      // Thread.sleep(3000);
      //driver.close();
 	}
