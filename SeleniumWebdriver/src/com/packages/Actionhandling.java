f5f155373b72c01145f339b0ad6b94c81833b85c
vipulkhutale12 <vipulkhutale1212>
2020-09-13 11:56:37 +0530

    h

--- SeleniumWebdriver/src/com/packages/Actionhandling.java
+++ SeleniumWebdriver/src/com/packages/Actionhandling.java
@@ -9,20 +9,20 @@ import org.openqa.selenium.interactions.Actions;
 public class Actionhandling {
 
 	public static void main(String[] args) {
-		
+
 		System.setProperty("webdriver.chrome.driver","D:\\Selenium\\Software\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://msrtc.maharashtra.gov.in/");
-		
+
 		WebElement eng=driver.findElement(By.linkText("English"));
-          eng.click();
-		
-        WebElement service=driver.findElement(By.linkText("Services"));
-		  //service.click();
-		
+		      eng.click();
+
+		WebElement service=driver.findElement(By.linkText("Services"));
+		   //service.click();
+
 		Actions act=new Actions(driver);
-		
+
 		act.moveToElement(service).build().perform();
 	}
 
